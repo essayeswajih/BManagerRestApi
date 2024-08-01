31c7c4b28f0e3197e7d45b16dade70a8fdba7359
essayeswajih <wajihsayes@gmail.com>
2024-08-01 14:32:16 +0100

    newFactureA

--- src/main/java/org/example/gestionfactureapi/Controller/BonLivAController.java
+++ src/main/java/org/example/gestionfactureapi/Controller/BonLivAController.java
@@ -129,6 +129,7 @@ public class BonLivAController {
                 }
 
             }
+            fileService.createAndSavePDF(x);
             return ResponseEntity.ok(x);
         }catch (Exception e){
             return ResponseEntity.internalServerError().body(e.getMessage());
