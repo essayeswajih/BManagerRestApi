4aa97b0284fb5a09674737279dfcdcc3897a649a
essayeswajih <wajihsayes@gmail.com>
2024-08-06 15:52:57 +0100

    orderByDes

--- src/main/java/org/example/gestionfactureapi/pdf/PDFGeneration.java
+++ src/main/java/org/example/gestionfactureapi/pdf/PDFGeneration.java
@@ -208,7 +208,7 @@ public class PDFGeneration {
         addCellOfHeading(clientTbale, "Code TVA", normalFont, 1, 1);
         addCell(clientTbale, this.bon.getFournisseur().getMatriculeFiscale(), normalFont, 2, 1);
         headerTable.addCell(companyCell);
-        clientTbale.setSpacingBefore(52);
+        //clientTbale.setSpacingBefore(52);
         clientCell.addElement(clientTbale);
         clientCell.setRowspan(2);
         headerTable.addCell(clientCell);
