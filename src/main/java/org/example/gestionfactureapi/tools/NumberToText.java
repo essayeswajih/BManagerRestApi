7bd17f421a7a992c60fffd67edbd34fcc5a6562f
essayeswajih <wajihsayes@gmail.com>
2024-08-07 11:45:38 +0100

    exonere

--- src/main/java/org/example/gestionfactureapi/tools/NumberToText.java
+++ src/main/java/org/example/gestionfactureapi/tools/NumberToText.java
@@ -5,7 +5,8 @@ import java.text.DecimalFormat;
 public class NumberToText {
 
     private static final String[] units = {"", "un", "deux", "trois", "quatre", "cinq", "six", "sept", "huit", "neuf"};
-    private static final String[] tens = {"", "dix", "vingt", "trente", "quarante", "cinquante", "soixante", "soixante-dix", "quatre-vingt", "quatre-vingt-dix"};
+    private static final String[] teens = {"dix", "onze", "douze", "treize", "quatorze", "quinze", "seize", "dix-sept", "dix-huit", "dix-neuf"};
+    private static final String[] tens = {"", "dix", "vingt", "trente", "quarante", "cinquante", "soixante", "soixante", "quatre-vingt", "quatre-vingt"};
     private static final String[] hundreds = {"", "cent", "deux cents", "trois cents", "quatre cents", "cinq cents", "six cents", "sept cents", "huit cents", "neuf cents"};
 
     private double number;
@@ -55,15 +56,26 @@ public class NumberToText {
             number %= 100;
         }
         if (number >= 20) {
-            if (number == 80) {
-                words.append("quatre-vingts ");
-                number = 0;
+            if (number < 30) {
+                words.append(tens[2]).append("-").append(units[number - 20]).append(" ");
+            } else if (number < 40) {
+                words.append(tens[3]).append("-").append(units[number - 30]).append(" ");
+            } else if (number < 50) {
+                words.append(tens[4]).append("-").append(units[number - 40]).append(" ");
+            } else if (number < 60) {
+                words.append(tens[5]).append("-").append(units[number - 50]).append(" ");
+            } else if (number < 70) {
+                words.append(tens[6]).append("-").append(units[number - 60]).append(" ");
+            } else if (number < 80) {
+                words.append("soixante-").append(teens[number - 70]).append(" ");
+            } else if (number < 90) {
+                words.append(tens[8]).append("-").append(units[number - 80]).append(" ");
             } else {
-                words.append(tens[number / 10]).append(" ");
-                number %= 10;
+                words.append("quatre-vingt-").append(units[number - 90]).append(" ");
             }
+            number = 0;
         }
-        if (number > 0) {
+        if (number > 0 && number < 10) {
             words.append(units[number]).append(" ");
         }
 
@@ -78,5 +90,8 @@ public class NumberToText {
         return df.format(number);
     }
 
+    public static void main(String[] args) {
+        NumberToText converter = new NumberToText("1234.567");
+        System.out.println(converter.toText());
+    }
 }
-
