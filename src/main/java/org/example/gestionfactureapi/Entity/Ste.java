a1130ec1853b1f0b8436812e3dd31afd6d457e1c
essayeswajih <wajihsayes@gmail.com>
2024-08-14 10:45:50 +0100

    msg

--- src/main/java/org/example/gestionfactureapi/Entity/Ste.java
+++ src/main/java/org/example/gestionfactureapi/Entity/Ste.java
@@ -29,7 +29,7 @@ public class Ste {
     private String formeJuridique;
     private String matriculeFiscale;
     @JsonIgnore
-    @OneToMany(mappedBy = "ste", cascade = CascadeType.ALL, orphanRemoval = true)
+    @OneToMany(mappedBy = "ste", cascade = CascadeType.ALL)
     private List<User> users;
     @JsonIgnore
     @OneToMany(mappedBy = "ste", cascade = CascadeType.ALL)
